4ed0b30e31cf1b4ef47642e7bd655f9b7d680ad1
nagaraju <nagaraju@nagarajus-MacBook-Pro.local>
2022-12-06 10:01:09 +0530

    sys out added

--- railways/src/main/java/com/railways/RailwaysApplication.java
+++ railways/src/main/java/com/railways/RailwaysApplication.java
@@ -8,7 +8,10 @@ public class RailwaysApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RailwaysApplication.class, args);
+		
 		System.out.println("this is my railways application");
+		
+		System.out.println("Ticket Booking System Application Still in Development Stage..");
 	}
 
 }
